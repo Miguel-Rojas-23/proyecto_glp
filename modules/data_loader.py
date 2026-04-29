207fc144cdc672f6f66e5b533eba0580104f6754
Miguel-Rojas-23 <tmiguel.rojas23@gmail.com>
2026-04-29 08:51:25 -0500

    29.04.26 - 08:50

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -3,18 +3,17 @@ import streamlit as st
 from pathlib import Path
 import numpy as np
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_PATH = BASE_DIR / "data" / "dataset_final_glp.parquet"
+DATA_PATH = Path("data/dataset_final_glp.parquet")
 
 @st.cache_data(show_spinner=False)
 def cargar_datos():
 
     try:
-
         st.write("📂 Ruta:", DATA_PATH)
 
         if not DATA_PATH.exists():
             st.error(f"❌ Archivo no encontrado:\n{DATA_PATH}")
+            st.write("📁 Archivos en /data:", list(Path("data").glob("*")))
             return pd.DataFrame()
 
         df = pd.read_parquet(DATA_PATH)
@@ -26,8 +25,7 @@ def cargar_datos():
 
         if "precio_de_venta_(soles)" in df.columns:
             df["precio_log"] = np.log(
-                df["precio_de_venta_(soles)"]
-                .replace(0, np.nan)
+                df["precio_de_venta_(soles)"].replace(0, np.nan)
             ).fillna(0)
 
         st.success(f"✅ Dataset cargado: {len(df):,} registros")
@@ -35,7 +33,5 @@ def cargar_datos():
         return df
 
     except Exception as e:
-
         st.error(f"❌ Error cargando parquet:\n{e}")
-
         return pd.DataFrame()
